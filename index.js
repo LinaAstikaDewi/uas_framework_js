4a782f165788f0c8ca17f6de49fed1138eb5b9b7
Lina Astika Dewi <101871109+LinaAstikaDewi@users.noreply.github.com>
2023-01-16 21:03:35 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from "express";
-import cors from "cors";
-import ObatRoute from "./routes/ObatRoute.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import "bulma/css/bulma.css";
  
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(ObatRoute);
-app.listen(5000, ()=> console.log('Server up and running...'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
